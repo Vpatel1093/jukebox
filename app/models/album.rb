fda2e67258c3e641a791f009a70a75b5e9ec2bc3
Vishal <v.patel1093@gmail.com>
2017-10-09 23:15:56 -0400

    add validations to models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,7 @@
 class Album < ApplicationRecord
+  validates :name, presence: true, length: {maximum: 255}
+  validates :artist_name, presence: true, length: {maximum: 255}
+  validates :cover_photo_url, presence: true, length: {maximum: 255}
+
   has_many :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,21 @@
 class Song < ApplicationRecord
+  serialize :song_label
+
+  validates :song_name, presence: true, length: {maximum: 255}
+  validates :song_order, presence: true
+  validate :song_label_validation
+  validates :song_duration, presence: true, length: {maximum: 255}
+  validates_presence_of :album_id
+
   belongs_to :album
+
+  LABELS = ['explicit','upbeat']
+
+
+  # validate length and name of label
+  def song_label_validation
+    song_label.each do |label|
+      errors.add(:song_label, '"#{label}" is either too long or invalid') unless label.length < 255 && LABELS.include? label
+    end
+  end
 end
