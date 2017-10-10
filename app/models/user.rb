a450a9a55cd9811965a9b8ed7642aac93b2e6439
Vishal <v.patel1093@gmail.com>
2017-10-09 22:26:56 -0400

    create database, add database migrations, add associations to models, migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :favorite_songs, foreign_key: "song_id", class_name: "Song"
 end
---
+++ db/migrate/20171010022450_add_song_to_users.rb
@@ -0,0 +1,5 @@
+class AddSongToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_reference :users, :song, foreign_key: true
+  end
+end
